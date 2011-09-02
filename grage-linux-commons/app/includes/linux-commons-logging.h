a4fabe3c3cc59dd5c1ab1767dfee4f190827e14a
fderosa@gmail.com <fderosa@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-09-02 04:35:03 +0000

    Se optimizo el logging, para k no abra y cierre archivo. Pero principalmente para que no consuma memoria en forma exponencial.

--- grage-linux-commons/app/includes/linux-commons-logging.h
+++ grage-linux-commons/app/includes/linux-commons-logging.h
@@ -5,6 +5,8 @@
  * 
  *
  */
+#ifndef COMMONS_LOGGING_H
+#define COMMONS_LOGGING_H
 
 #define BUFFSIZE 2048
 
@@ -12,25 +14,31 @@
 #define LOGGING_LEVEL_INFO				2
 #define LOGGING_LEVEL_ERROR				1
 
-#define LOGGING_LEVEL_STRING_DEBUG		"debug"
-#define LOGGING_LEVEL_STRING_INFO		"info"
-#define LOGGING_LEVEL_STRING_ERROR		"error"
+#define DEBUG_LVL "DEBUG"
+#define INFO_LVL "INFO"
+#define ERROR_LVL "ERROR"
 
-	/**
-	 * Niveles de logging.
-	 */
-	void commons_logging_setLoggingLevelEnabled(int level);
-	int  commons_logging_getLoggingLevelEnabled();
-	Boolean commons_logging_isDebugEnabled();
-	Boolean commons_logging_isInfoEnabled ();
+/**
+ * Niveles de logging.
+ */
+void commons_logging_setLoggingLevelEnabled(int level);
+int commons_logging_getLoggingLevelEnabled();
+Boolean commons_logging_isDebugEnabled();
+Boolean commons_logging_isInfoEnabled();
 
-	/**
-	 * Strings de logging.
-	 */
-	char * commons_logging_getLogString  (char * , char * , char * );
-	void   commons_logging_writeLogString(char * , char * );
+/**
+ * Strings de logging.
+ */
+char *commons_logging_getLogString(char *, char *, char *);
+void commons_logging_writeLogString(char *, char *);
+void commons_logging_disablePrintf();
+void commons_logging_initLog(char *);
+void commons_logging_regSignals();
+void commons_logging_shutdownLog();
 
-	void commons_logging_println (char * proc , char * type , char * description );
-	void commons_logging_logInfo (char * proc , char * description );
-	void commons_logging_logError(char * proc , char * description );
-	void commons_logging_logDebug(char * proc , char * description );
+void commons_logging_println(char *, char *, char *);
+void commons_logging_logInfo(char *, char *);
+void commons_logging_logError(char *, char *);
+void commons_logging_logDebug(char *, char *);
+
+#endif
--- grage-linux-commons/app/src/misc/linux-commons-logging.c
+++ grage-linux-commons/app/src/misc/linux-commons-logging.c
@@ -15,89 +15,136 @@
 #include <time.h>
 #include <sys/timeb.h>
 #include <pthread.h>
+#include <signal.h>
 
 #include "linux-commons.h"
 #include "linux-commons-logging.h"
 
+Boolean printfEnabled = TRUE;
 int levelEnabled;
+FILE *logfile;
+pthread_mutex_t logmutex=PTHREAD_MUTEX_INITIALIZER;
 
-	void commons_logging_setLoggingLevelEnabled(int level){
-		levelEnabled = level;
+
+void commons_logging_setLoggingLevelEnabled(int level) {
+	levelEnabled = level;
+}
+
+void commons_logging_disablePrintf(){
+	printfEnabled = FALSE;
+}
+
+int commons_logging_getLoggingLevelEnabled() {
+	return levelEnabled;
+}
+
+Boolean commons_logging_isDebugEnabled() {
+	if (levelEnabled >= LOGGING_LEVEL_DEBUG) {
+		return TRUE;
+	} else {
+		return FALSE;
 	}
+}
 
-	int commons_logging_getLoggingLevelEnabled(){
-		return levelEnabled;
+Boolean commons_logging_isInfoEnabled() {
+	if (levelEnabled >= LOGGING_LEVEL_INFO) {
+		return TRUE;
+	} else {
+		return FALSE;
 	}
+}
 
-	Boolean commons_logging_isDebugEnabled(){
-		if(levelEnabled >= LOGGING_LEVEL_DEBUG){
-			return TRUE;
-		}else{
-			return FALSE;
-		}
+char * commons_logging_getLogString(char * procName, char * type, char * desc) {
+	char * auxLogString = (char *) malloc(BUFFSIZE);
+	char * logString = NULL;
+	char inst[12];
+	int pid = getpid();
+	pthread_t tid = pthread_self();
+	struct timeb tmili;
+
+	time_t curtime = time(NULL);
+	ftime(&tmili);
+	struct tm * loctime = localtime(&curtime);
+	sprintf(inst, "%d:%d:%d:%d", loctime->tm_hour, loctime->tm_min, loctime->tm_sec, tmili.millitm);
+	desc = commons_string_trimd(desc);
+	sprintf(auxLogString, "[%s] [Proc:%s] [PID:%d] [TID:%d] [Type:%s] %s\r\n", inst, procName, pid, (int) tid, type, desc);
+	free(desc);
+	//Por si la cadena ocupa menos de 2048 caracteres.
+	logString = strdup(auxLogString);
+	free(auxLogString);
+
+	return logString;
+}
+
+void commons_logging_initLog(char * procName) {
+	char logname[20];
+
+	bzero(logname, sizeof(logname));
+	sprintf(logname, "app/logs/%s.log", procName);
+
+	logfile = fopen(logname, "a+");
+
+	if (logfile == NULL)
+		perror("No se pudo abrir el logfile.\n");
+
+	//seteo el nivel info por default
+	commons_logging_setLoggingLevelEnabled(LOGGING_LEVEL_INFO);
+}
+
+void commons_logging_regSignals() {
+	signal(SIGINT, commons_logging_shutdownLog);
+	signal(SIGKILL, commons_logging_shutdownLog);
+	signal(SIGTERM, commons_logging_shutdownLog);
+	signal(SIGSTOP, commons_logging_shutdownLog);
+}
+
+void commons_logging_shutdownLog() {
+	fclose(logfile);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGKILL, SIG_DFL);
+	signal(SIGTERM, SIG_DFL);
+	signal(SIGSTOP, SIG_DFL);
+	kill(getpid(),SIGINT);
+}
+
+void commons_logging_writeLogString(char * procName, char * logString) {
+	pthread_mutex_lock(&logmutex);
+	fwrite(logString, strlen(logString), 1, logfile);
+	fflush(logfile);
+	fsync(fileno(logfile));
+	pthread_mutex_unlock(&logmutex);
+}
+
+void commons_logging_println(char * proc, char * type, char * description) {
+	char * logString = commons_logging_getLogString(proc, type, description);
+	printf("%s", logString);
+	free(logString);
+}
+
+void commons_logging_logInfo(char * proc, char * description) {
+	if (commons_logging_isInfoEnabled()) {
+		char * logString = commons_logging_getLogString(proc, INFO_LVL, description);
+		if(printfEnabled)
+			printf("%s", logString);
+		commons_logging_writeLogString(proc, logString);
+		free(logString);
 	}
+}
 
-	Boolean commons_logging_isInfoEnabled(){
-		if(levelEnabled >= LOGGING_LEVEL_INFO){
-			return TRUE;
-		}else{
-			return FALSE;
-		}
+void commons_logging_logError(char * proc, char * description) {
+	char * logString = commons_logging_getLogString(proc, ERROR_LVL, description);
+	if(printfEnabled)
+		printf("%s", logString);
+	commons_logging_writeLogString(proc, logString);
+	free(logString);
+}
+
+void commons_logging_logDebug(char * proc, char * description) {
+	if (commons_logging_isDebugEnabled()) {
+		char * logString = commons_logging_getLogString(proc, DEBUG_LVL, description);
+		if(printfEnabled)
+			printf("%s", logString);
+		commons_logging_writeLogString(proc, logString);
+		free(logString);
 	}
-	
-	char * commons_logging_getLogString(char * procName , char * type, char * desc) {
-		char * logString = (char *)malloc(BUFFSIZE);
-		char inst[12];
-		int pid = getpid();
-		pthread_t tid = pthread_self();
-		struct timeb tmili;
-
-		time_t curtime = time(NULL);
-		ftime (&tmili);
-		struct tm * loctime = localtime(&curtime);
-		sprintf( inst , "%d:%d:%d:%d" , loctime->tm_hour , loctime->tm_min , loctime->tm_sec , tmili.millitm );
-
-		sprintf( logString , "[%s] [Proc:%s] [PID:%d] [TID:%d] [Type:%s] %s\r\n" , inst,procName , pid , (int)tid , type , desc );
-
-		return logString;
-	}
-	
-	void commons_logging_writeLogString(char * procName , char * logString ){
-		FILE *logfile;
-		char logname[20];
-
-		bzero( logname , sizeof(logname) );
-		sprintf( logname , "app/logs/%s.log" , procName );
-
-		logfile = fopen( logname , "a+");
-
-		if ( logfile == NULL ) {
-			printf("No se pudo abrir el logfile.\n");
-		} else {
-			fwrite( logString, strlen(logString) , 1 , logfile );
-			fclose(logfile);
-		}
-	}
-	
-	void commons_logging_println(char * proc , char * type , char * description ){
-		printf("%s" , commons_logging_getLogString(proc , type , description) );
-	}
-	
-	void commons_logging_logInfo(char * proc , char * description ){
-		if(commons_logging_isInfoEnabled()){
-			commons_logging_println(proc , "INFO" , description );
-			commons_logging_writeLogString(proc , commons_logging_getLogString(proc , "INFO" , description));
-		}
-	}
-
-	void commons_logging_logError(char * proc , char * description ){
-		commons_logging_println(proc , "ERROR" , description );
-		commons_logging_writeLogString(proc , commons_logging_getLogString(proc , "ERROR" , description));
-	}
-
-	void commons_logging_logDebug(char * proc , char * description ){
-		if(commons_logging_isDebugEnabled()){
-			commons_logging_println(proc , "DEBUG" , description );
-			commons_logging_writeLogString(proc , commons_logging_getLogString(proc , "DEBUG" , description));
-		}
-	}
+}
