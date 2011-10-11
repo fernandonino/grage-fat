fb7267d86f5a9b3333e461a9007c503653a45ea0
joaquinperezbay@gmail.com <joaquinperezbay@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-10-11 00:48:55 +0000

    Consola ppd parser terminado

--- grage-ppd-console/app/includes/ppd_console_interpreter.h
+++ grage-ppd-console/app/includes/ppd_console_interpreter.h
@@ -5,9 +5,17 @@
  *      Author: joaquin
  */
 
+#include "linux-commons.h"
+
 #ifndef PPD_CONSOLE_INTERPRETER_H_
 #define PPD_CONSOLE_INTERPRETER_H_
 
+	String ppd_console_parseCMD(char * comando);
+	String ppd_console_get_cmd_parameter(char * comando, uint32 cmd_length);
+	uint32 ppd_console_obtenerNroParametros(char * comando);
+	void ppd_console_llenarVector(char * comando, uint32 nro_parametros);
+	void ppd_console_info();
+	void ppd_console_clean(uint32 clusterInicial, uint32 clusterFinal);
+	void ppd_console_trace(uint32 sectorPedido);
 	void ppd_console_interpreter();
-
 #endif /* PPD_CONSOLE_INTERPRETER_H_ */
--- grage-ppd-console/app/src/ppd_console_interpreter.c
+++ grage-ppd-console/app/src/ppd_console_interpreter.c
@@ -37,11 +37,14 @@ String ppd_console_get_cmd_parameter(char * comando, uint32 cmd_length){
 	parametro = (String)malloc(255);
 	memset(parametro, '\0', sizeof parametro);
 
-	while(comando[i] != ')' && i < (250 - cmd_length)){
+	while(comando[i] != ')' && comando[i] != ' ' && i < (250 - cmd_length)
+			&& comando[i] >= 48 && comando[i] <= 57)
+	{
 		parametro[j] = comando[i];
 		i++;
 		j++;
 	}
+	if(comando[i] >= 57 || comando[i] <= 48) puts("Valor de Cluster ingresado incorrecto");
 	return parametro;
 }
 
@@ -53,41 +56,62 @@ uint32 ppd_console_obtenerNroParametros(char * comando){
 		if(comando[i] == ' ') cant++;
 		i++;
 	}
-	if(cant > 0) return ++cant;
-	else{
-		puts("trace debe reecibir parametros");
+	printf("AAAAAAAAAAa %d\n", cant);
+	if(cant > 0 && cant < 5 &&comando[6] != ' ') {
+		cant++;
 		return cant;
 	}
+	if(cant == 0 && atoi(ppd_console_get_cmd_parameter(comando, strlen(ppd_console_parseCMD(comando)))) > 0){
+		return 1;
+	}
+	if(comando[6] == ' '){
+		puts("El primer parametro no debe ser vacio");
+		return 0;
+	}
+	if (cant == 0 && atoi(ppd_console_get_cmd_parameter(comando, strlen(ppd_console_parseCMD(comando)))) <= 0){
+		puts("trace debe recibir parametros");
+		return 0;
+	}
+	if(cant >= 5){
+		puts("El maximo de clusters es 5");
+		return 0;
+	}
+	return 0;
 }
 
 void ppd_console_llenarVector(char * comando, uint32 nro_parametros){
-	uint32 i = 6;
-	uint32 j = 0;
-	uint32 k = 0;
-	char aux[6];
-	while(comando[i] != ')' && i < 244 && j <= nro_parametros){
-		while(comando[i] != ' ' && comando[i] != ')'){
-			aux[k] = comando[i];
-			k++;
+	if(nro_parametros != 0){
+		uint32 i = 6;
+		uint32 j = 0;
+		uint32 k = 0;
+		char aux[6];
+		while(comando[i] != ')' && i < 244 && j < nro_parametros){
+			while(comando[i] >= 48 && comando[i] <= 57){
+				aux[k] = comando[i];
+				k++;
+				i++;
+			}
+			k = 0;
+			sectores[j] = atoi(aux);
+			j++;
 			i++;
 		}
-		k = 0;
-		sectores[j] = atoi(aux);
-		j++;
-		i++;
 	}
 }
 
 void ppd_console_info(){
-
+	puts("console info");
 }
 
 void ppd_console_clean(uint32 clusterInicial, uint32 clusterFinal){
-
+	puts("console clean");
+	printf("clusterInicial %d\n", clusterInicial);
+	printf("clusterFinal %d\n", clusterFinal);
 }
 
 void ppd_console_trace(uint32 sectorPedido){
-
+	puts("console trace");
+	printf("sectorPedido: %d\n", sectorPedido);
 }
 
 void ppd_console_interpreter(){
@@ -98,23 +122,35 @@ void ppd_console_interpreter(){
 	while(!feof(stdin)){
 		printf(">");
 		fgets(buffer, 250, stdin);
-		printf(">%s\n", buffer);
 		cmd = ppd_console_parseCMD(buffer);
-		if(!strcmp(cmd, "info")) ppd_console_info();
-		if(!strcmp(cmd, "clean")){
-			parameter  = ppd_console_get_cmd_parameter(buffer, strlen(cmd));
-			parameter2 = ppd_console_get_cmd_parameter(buffer, (strlen(cmd) + 1 + strlen(parameter)));
-			ppd_console_clean(atoi(parameter), atoi(parameter2));
+		//INFO
+		if(!strcmp(cmd, "info"))ppd_console_info();
+		//CLEAN
+		if(!strcmp(cmd, "clean") && buffer[5]!= ' '){
+			if(buffer[5]=='(' && buffer[6]!=')'){
+				parameter  = ppd_console_get_cmd_parameter(buffer, strlen(cmd));
+				parameter2 = ppd_console_get_cmd_parameter(buffer, (strlen(cmd) + 1 + strlen(parameter)));
+				if(atoi(parameter2)>0) ppd_console_clean(atoi(parameter), atoi(parameter2));
+				else puts("Indique clusters a limpiar");
+				bzero(parameter, sizeof(parameter));
+				bzero(parameter2,sizeof(parameter2));
+			}
+			else puts("Indique clusters a limpiar");
 		}
-		if(!strcmp(cmd, "trace")){
-			uint32 nro_parametros, i = 0;
+		//TRACE
+		if(!strcmp(cmd, "trace") && buffer[5]!= ' '){
+			uint32 nro_parametros;
+			uint32 i = 1;
 			nro_parametros = ppd_console_obtenerNroParametros(buffer);
 			ppd_console_llenarVector(buffer, nro_parametros);
 			while(i <= nro_parametros){
-				ppd_console_trace(sectores[i]);
+				ppd_console_trace(sectores[i - 1]);
 				i++;
 			}
 		}
-		if(strcmp(cmd, "info") && strcmp(cmd, "clean") && strcmp(cmd, "trace")) puts("Comando incorrecto");
+
+		if((strcmp(cmd, "info") && strcmp(cmd, "clean") && strcmp(cmd, "trace")) || buffer[5]== ' ')
+			puts("Comando incorrecto");
+		bzero(buffer, sizeof(buffer));
 	}
 }
