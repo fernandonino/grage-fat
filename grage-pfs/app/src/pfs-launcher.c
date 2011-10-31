065edb06ed6004dee60dd3355e38e04fbea89645
kronleuchter85@gmail.com <kronleuchter85@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-10-31 02:34:52 +0000

    mejoramiento del pfs

--- grage-pfs/app/src/pfs-launcher.c
+++ grage-pfs/app/src/pfs-launcher.c
@@ -8,18 +8,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <linux-commons-logging.h>
+#include <linux-commons-socket.h>
+#include <nipc-messaging.h>
+
 #include "pfs-configuration.h"
 #include "pfs-console.h"
 #include "pfs-fat-utils.h"
+
+
+
+
+	void pfs_test_villa_21(){
+		char * host = pfs_configuration_getDeviceAddress();
+		char * port = pfs_configuration_getDevicePort();
+		ListenSocket lsocket = commons_socket_openClientConnection(host , port);
+
+		RuntimeErrorValidator * validator = commons_errors_buildSuccessValidator();
+		nipc_sendHandshake(lsocket , NIPC_PROCESS_ID_PFS , validator);
+		NipcMessage message = nipc_receiveHandshake(lsocket , validator);
+
+		if(message.header.responseCode == NIPC_RESPONSE_CODE_ERROR
+				|| commons_errors_hasError(validator))
+			exit(1);
+
+		DiskSector sector;
+		sector.sectorNumber = 1;
+		strcpy(sector.sectorContent , "hola mundo");
+
+		message = nipc_mbuilder_buildNipcMessage();
+		message = nipc_mbuilder_addOperationId(message , NIPC_OPERATION_ID_PUT_SECTORS);
+		message = nipc_mbuilder_addMessageType(message , NIPC_MESSAGE_TYPE_SECTOR_DISK);
+		message = nipc_mbuilder_addDiskSector(message , sector);
+
+		nipc_messaging_send(lsocket , message);
+
+	}
+
+
+
+
 	void pfs_launcher_initialize(){
-		log_create("pfs","../logs/pfs.log",INFO|DEBUG|WARNING|ERROR,M_CONSOLE_DISABLE);
+		log_create("pfs","/opt/grage-repository/logs/pfs.log",INFO|DEBUG|WARNING|ERROR,M_CONSOLE_DISABLE);
 		pfs_configuration_initialize();
-		pfs_console_initialize();
+		//pfs_console_initialize();
 	}
 
 
 	void pfs_launcher_launch(){
-
+		pfs_test_villa_21();
 	}
 
 	void pfs_launcher_exit(){
@@ -42,5 +78,6 @@
 
 		pfs_launcher_launch();
 
+		pfs_launcher_exit();
 		return EXIT_SUCCESS;
 	}
