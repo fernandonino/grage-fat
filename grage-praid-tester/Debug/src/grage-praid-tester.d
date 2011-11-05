5a6b93f31d42e630ef34fba644ee06f3a9128e6f
kronleuchter85@gmail.com <kronleuchter85@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-11-05 19:11:04 +0000

    cliente de pruebas para el praid (ex integration)

---
+++ grage-praid-tester/Debug/src/grage-praid-tester.d
@@ -0,0 +1,23 @@
+src/grage-praid-tester.d src/grage-praid-tester.o: \
+ ../src/grage-praid-tester.c \
+ /opt/grage-repository/includes/nipc-messaging.h \
+ /opt/grage-repository/includes/grage-commons.h \
+ /opt/grage-repository/includes/linux-commons.h \
+ /opt/grage-repository/includes/linux-commons-socket.h \
+ /opt/grage-repository/includes/linux-commons-errors.h \
+ /opt/grage-repository/includes/linux-commons-logging.h \
+ /opt/grage-repository/includes/linux-commons.h
+
+/opt/grage-repository/includes/nipc-messaging.h:
+
+/opt/grage-repository/includes/grage-commons.h:
+
+/opt/grage-repository/includes/linux-commons.h:
+
+/opt/grage-repository/includes/linux-commons-socket.h:
+
+/opt/grage-repository/includes/linux-commons-errors.h:
+
+/opt/grage-repository/includes/linux-commons-logging.h:
+
+/opt/grage-repository/includes/linux-commons.h:
