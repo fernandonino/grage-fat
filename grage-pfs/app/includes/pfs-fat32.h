ab08d0d0259e1f81b6b0008a83bc846b6fc2d6db
kronleuchter85@gmail.com <kronleuchter85@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-11-06 03:49:05 +0000

    fat sin volume ni bpb

--- grage-pfs/app/includes/pfs-fat32.h
+++ grage-pfs/app/includes/pfs-fat32.h
@@ -34,57 +34,6 @@
 
 
 	typedef struct {
-		uint8_t BS_jmpBoot[3];
-		uint8_t BS_OEMName[8];
-		uint16_t BPB_BytsPerSec;
-		uint8_t BPB_SecPerClus;
-		uint16_t BPB_ResvdSecCnt;
-		uint8_t BPB_NumFATs;
-		uint16_t BPB_RootEntCnt;
-		uint16_t BPB_TotSec16;
-		uint8_t BPB_Media;
-		uint16_t BPB_FATSz16;
-		uint16_t BPB_SecPerTrk;
-		uint16_t BPB_NumHeads;
-		uint32_t BPB_HiddSec;
-		uint32_t BPB_TotSec32;
-		uint32_t BPB_FATSz32;
-		uint16_t BPB_ExtFlags;
-		uint16_t BPB_FSVer;
-		uint32_t BPB_RootClus;
-		uint16_t BPB_FSInfo;
-		uint16_t BPB_BkBootSec;
-		uint8_t BPB_Reserved[12];
-		uint8_t BS_DrvNum;
-		uint8_t BS_Reserved1;
-		uint8_t BS_BootSig;
-		uint32_t BS_VolID;
-		uint8_t BS_VolLab[11];
-		uint8_t BS_FilSysType[8];
-	} BPB;
-
-
-
-	typedef struct {
-		uint16_t bps; 			// bytes per sector
-		uint16_t spc; 			// sectors per cluster
-		uint16_t bpc; 			// bytes per cluster
-		uint16_t fds; 			// first data sector
-		uint16_t rsv;			// reserved sector count
-
-		uint8_t	fatQty; 		// quantity of FAT tables
-		uint16_t fatSize; 		// size of FAT in sectors
-		uint16_t fatStartSector; // sector donde comienza la fat
-
-		uint32_t clusters;		// cluster quantity
-		uint32_t sectors;		// sector quantity
-		uint32_t dataSectors;	// data sector quantity
-		uint32_t root;			// cluster number of root directory
-
-		int32_t disk;			// disk file descriptor - solo ese usa en los tests
-	} Volume;
-
-	typedef struct {
 		uint8_t DIR_Name[11];
 		uint8_t DIR_Attr;
 		uint8_t DIR_NTRes;
