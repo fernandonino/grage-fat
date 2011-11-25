816cc972c700226c8ee507e6552ea32f710a99be
kronleuchter85@gmail.com <kronleuchter85@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-11-25 02:47:29 +0000

    tag de version minimamente estable

---
+++ grage-commons/app/src/misc/linux-commons-queue.c
@@ -0,0 +1,75 @@
+/*
+ * linux-commons-queue.c
+ *
+ *  Created on: 26/09/2011
+ *      Author: gonzalo
+ */
+
+#include "linux-commons-queue.h"
+
+
+
+	Object commons_queue_get(Queue aQueue){
+		if(aQueue == NULL || aQueue->elements == NULL)
+			return NULL;
+
+		Node * theFirst = aQueue->elements;
+		aQueue->elements = theFirst->next;
+		aQueue->size = --aQueue->size;
+
+		Object theData = theFirst->data;
+		commons_misc_doFreeNull(theFirst);
+
+		return theData;
+	}
+
+
+	void commons_queue_put( Queue aQueue , Object anElement){
+		commons_list_addNode(aQueue , anElement);
+	}
+
+
+
+	void commons_queue_destroy(Queue aQueue){
+		printf("fabi se la come!");
+	}
+
+
+	/*
+	 * Crea una cola loca :D...
+	 */
+	Queue commons_queue_buildQueue(
+			Boolean (*aEqualityCriteria)(Object , Object)){
+
+		ThreadMutex m = PTHREAD_MUTEX_INITIALIZER;
+
+		Queue theQueue = malloc(sizeof(L));
+		theQueue->size = 0;
+		theQueue->elements = NULL;
+		theQueue->mutex = m;
+		theQueue->equalityCriteria = aEqualityCriteria;
+		theQueue->sortingCriteria = commons_list_ORDER_ALWAYS_LAST;
+		return theQueue;
+	}
+
+	Queue commons_queue_buildQueueWithSortingCriteria(
+			Boolean (*aEqualityCriteria)(Object , Object),
+			Boolean (*sortingCriteria)(Object , Object)){
+
+		ThreadMutex m = PTHREAD_MUTEX_INITIALIZER;
+
+		Queue theQueue = malloc(sizeof(L));
+		theQueue->size = 0;
+		theQueue->elements = NULL;
+		theQueue->mutex = m;
+		theQueue->equalityCriteria = aEqualityCriteria;
+		theQueue->sortingCriteria = commons_list_ORDER_ALWAYS_LAST;
+		return theQueue;
+	}
+
+	Boolean commons_queue_isEmpty(Queue aQueue){
+		if(aQueue == NULL)
+			return TRUE;
+		return (aQueue->size == 0);
+	}
+
