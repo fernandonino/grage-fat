33586a76a25c4fc2e95d1ce223c00cb2e2861902
kronleuchter85@gmail.com <kronleuchter85@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-12-03 09:09:42 +0000

    version estable

---
+++ SEBA_GAY/app/src/planifier/ppd-algorythm-strategy.c
@@ -0,0 +1,36 @@
+
+
+
+#include "linux-commons-queue.h"
+#include "ppd-queues.h"
+#include "ppd-console-entreypoint.h"
+#include "ppd-utils.h"
+#include <math.h>
+	uint32 currentPossition;
+//	void ppd_alg_setCurrentPossition(uint32 posicion){
+//		currentPossition = posicion;
+//	}
+	Boolean ppd_alg_planif_strategy_look(Object obj1, Object obj2){
+		Job * oldJob = (Job *) obj1;
+		Job * newJob = (Job *) obj2;
+		currentPossition = ppd_utils_get_sector_from_sectorofcilinder(
+				ppd_console_entrypoint_getPosicionCabezal().sectorNumber,
+				ppd_console_entrypoint_getPosicionCabezal().pista) ;
+		if ( abs(currentPossition - oldJob->sectorId) >
+			abs(currentPossition - newJob->sectorId) && newJob->sectorId >= currentPossition){
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	Boolean ppd_alg_planif_strategy_sstf(Object obj1, Object obj2){
+		Job * oldJob = (Job *) obj1;
+		Job * newJob = (Job *) obj2;
+		currentPossition = ppd_utils_get_sector_from_sectorofcilinder(
+				ppd_console_entrypoint_getPosicionCabezal().sectorNumber,
+				ppd_console_entrypoint_getPosicionCabezal().pista) ;
+		if ( abs(currentPossition - oldJob->sectorId) > abs(currentPossition - newJob->sectorId)){
+			return TRUE;
+		}
+		return FALSE;
+	}
