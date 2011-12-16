1ce93268c9b08690a186c834f6f7b4a61105b9b8
joaquinperezbay@gmail.com <joaquinperezbay@gmail.com@36ce5b7e-71da-a8b3-8903-ea55245d5ff9>
2011-12-16 10:01:36 +0000

    Cambios en el commons list para permitir el uso de la cache.

--- grage-commons/app/src/misc/linux-commons-list.c
+++ grage-commons/app/src/misc/linux-commons-list.c
@@ -66,22 +66,22 @@
 				aux = aux->next;
 			}
 
-			if(aux->next == NULL){
-				aux->next = n;
-			}else{
-				n->next = aux->next;
-				aux->next = n;
-			}
-
-//			if(!l->sortingCriteria(aux->data , n->data)){
-//				n->next = aux;
-//				l->elements = n;
+//			if(aux->next == NULL){
+//				aux->next = n;
 //			}else{
-//				if( aux->next != NULL){
-//					n->next = aux->next;
-//				}
+//				n->next = aux->next;
 //				aux->next = n;
 //			}
+
+			if(!l->sortingCriteria(aux->data , n->data)){
+				n->next = aux;
+				l->elements = n;
+			}else{
+				if( aux->next != NULL){
+					n->next = aux->next;
+				}
+				aux->next = n;
+			}
 		}
 
 		l->size++;
